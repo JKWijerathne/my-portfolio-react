1cb917cec03de67a231040dccd15970ebdd3873a
Janani Wijerathne <jkwijerathne7@gmail.com>
2026-04-21 22:58:40 +0530

    Add home and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import Navbar from './components/Navbar/navbar';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      {/* Sections will go here */}
-      <section id="home" style={{height: '100vh'}}>Home Section</section>
+      <Home />
       <section id="skills" style={{height: '100vh'}}>Skills Section</section>
       <section id="projects" style={{height: '100vh'}}>Projects Section</section>
       <section id="blogs" style={{height: '100vh'}}>Blogs Section</section>
       <section id="education" style={{height: '100vh'}}>Education Section</section>
       <section id="contact" style={{height: '100vh'}}>Contact Section</section>
     </div>
+
   );
 }
 
