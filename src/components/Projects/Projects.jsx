a048fdb4d133e594af93dd67dd08a95f35d42dc0
Janani Wijerathne <jkwijerathne7@gmail.com>
2026-04-30 01:53:17 +0530

    update css styling

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -20,10 +20,10 @@ const Projects = () => {
                             and secure JWT-based authentication.
                         </p>
                         <div className="projectTechnologies">
-                            <span className="techTag">Java</span>
+                            <span className="techTag">JavaScript</span>
                             <span className="techTag">Spring Boot</span>
                             <span className="techTag">React</span>
-                            <span className="techTag">MySQL</span>
+                            <span className="techTag">PostgreSQL</span>
                             <span className="techTag">JWT</span>
                         </div>
                         <a href="https://github.com/JKWijerathne/architecture_project" target="_blank" rel="noreferrer" className="githubLink">
